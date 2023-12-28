d376eff7a28635cf119ab8b8399868f8b99aa29c
DariaPetre <96527117+DariaPetre@users.noreply.github.com>
2023-12-28 14:52:11 +0200

    Add files via upload

---
+++ Set1 ex3/Set1 ex3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace Set1_ex3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //*****~n se divide cu k~******
+            Console.Write("n=");
+            double n = double.Parse(Console.ReadLine());
+
+            Console.Write("k=");
+            double k = double.Parse(Console.ReadLine());
+
+            if(k == 0 )
+            {
+                throw new ArgumentException("k nu poate fi 0.");
+            }
+
+            if(n % k == 0)
+            {
+                Console.WriteLine("n se divide cu k.");
+            }
+            else
+            {
+                Console.WriteLine("n NU se divide cu k.");
+            }
+
+        }
+    }
+}
