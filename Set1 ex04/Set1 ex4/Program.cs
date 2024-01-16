ff97464d1bd48e70ac7d00b57a564aefbcd1c923
DariaPetre <96527117+DariaPetre@users.noreply.github.com>
2024-01-16 20:05:23 +0200

    Add files via upload

---
+++ Set1 ex04/Set1 ex4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Set1_ex4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           //*******~  y este an bisect?  ~******
+           int y = Convert.ToInt32(Console.ReadLine());
+            bool anBisect = DateTime.IsLeapYear(y);
+            if (anBisect)
+            {
+                Console.WriteLine("y este an bisect.");
+            }
+            else
+            {
+                Console.WriteLine("y NU este an bisect.");
+            }
+
+        }
+    }
+}
