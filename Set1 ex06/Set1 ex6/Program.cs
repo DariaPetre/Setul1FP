f0d71299f1318e5eee0c31528e4e9c168449c7ab
DariaPetre <96527117+DariaPetre@users.noreply.github.com>
2024-01-16 20:06:40 +0200

    Add files via upload

---
+++ Set1 ex06/Set1 ex6/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Set1_ex6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("a=");
+            float a = float.Parse(Console.ReadLine());
+
+            Console.Write("b=");
+            float b = float.Parse(Console.ReadLine());
+
+            Console.Write("c=");
+            float c = float.Parse(Console.ReadLine());
+
+            if (a < b + c && b < a + c && c < a + b)
+            {
+                Console.WriteLine("Valorile a,b,c pot reprezenta lungimile laturilor unui triunghi.");
+            }
+            else
+            {
+                Console.WriteLine("Valorile a,b,c NU pot reprezenta lungimile laturilor unui triunghi.");
+            }
+        }
+    }
+}
