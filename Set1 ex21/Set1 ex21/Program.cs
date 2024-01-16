779d7c2d9874e3c105bb312761a6662d4b8d413a
DariaPetre <96527117+DariaPetre@users.noreply.github.com>
2024-01-16 20:18:19 +0200

    Add files via upload

---
+++ Set1 ex21/Set1 ex21/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Set1_ex21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            int number = random.Next(1,1025);
+
+            bool AiGhicit = false;
+
+            int NrDeIncercari = 0;
+
+            while (!AiGhicit)
+            {   
+                Console.Write("Ghiceste numarul:");
+                int guess = int.Parse(Console.ReadLine());
+                NrDeIncercari++;
+                Console.WriteLine($"Numarul este mai mare sau egal decat {guess}?");
+                if (number > guess)
+                    Console.WriteLine("DA");
+                else if (number < guess)
+                    Console.WriteLine("NU");
+                else
+                {
+                    Console.WriteLine($"Ai Ghicit, numarul este {number}.");
+                    Console.WriteLine($"Numarul de incercari este {NrDeIncercari}.");
+                    AiGhicit = true;
+                }
+               
+            }
+
+        }
+    }
+}
