9c499421e375150596578e7f4261305da581f2d4
DariaPetre <96527117+DariaPetre@users.noreply.github.com>
2024-01-16 20:06:04 +0200

    Add files via upload

---
+++ Set1 ex05/Set1 ex5/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Set1_ex5
+{
+    internal class Program
+    {   //a k cifra din nr n 
+        static void Main(string[] args)
+        {
+            Console.Write("n=");
+            int n=int.Parse(Console.ReadLine());
+
+            Console.Write("k=");
+            int k = int.Parse(Console.ReadLine());
+
+            while (k > 1)
+            {
+                k--;
+                n /= 10;
+            }
+            Console.WriteLine($"{n % 10}");
+
+        }
+    }
+}
