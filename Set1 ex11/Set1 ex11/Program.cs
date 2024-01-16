c28793f4cd30f1779cb53fc967fc08e6e8028a02
DariaPetre <96527117+DariaPetre@users.noreply.github.com>
2024-01-16 20:10:15 +0200

    Add files via upload

---
+++ Set1 ex11/Set1 ex11/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Dynamic;
+using System.Formats.Asn1;
+
+namespace Set1_ex11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("nr=");
+            double nr = Convert.ToDouble(Console.ReadLine());
+
+            string strNr  = Convert.ToString(nr);
+            int length = strNr.Length;
+
+            for(int i = length - 1; i >= 0; i--)
+            {
+                if (strNr[i] != '.')
+                  Console.Write($"{strNr[i]} ");
+            }
+
+        }
+    }
+}
