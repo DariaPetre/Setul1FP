644d8d2c0b77944d8169a7dceb88dead467d0573
DariaPetre <96527117+DariaPetre@users.noreply.github.com>
2024-01-16 20:15:31 +0200

    Add files via upload

---
+++ Set1 ex17/Set1 ex17/Program.cs
@@ -0,0 +1,28 @@
+﻿
+namespace Set1_ex17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("a=");
+            int a = int.Parse(Console.ReadLine());
+            int CopiaLuia = a;
+
+            Console.Write("b=");
+            int b = int.Parse(Console.ReadLine());
+            int CopiaLuib = b;
+            while (b > 0) 
+            {
+                int r = a % b;
+                a = b;
+                b = r;
+            }
+
+            int cmmdc = a;
+            int cmmmc = (CopiaLuia * CopiaLuib) / cmmdc;
+
+            Console.WriteLine($"cmmdc={cmmdc} cmmmc={cmmmc}");
+        }
+    }
+}
