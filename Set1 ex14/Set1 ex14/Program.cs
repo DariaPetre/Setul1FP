2797cacba84930ff06ef9ddc535edf351c279f55
DariaPetre <96527117+DariaPetre@users.noreply.github.com>
2024-01-16 20:13:25 +0200

    Add files via upload

---
+++ Set1 ex14/Set1 ex14/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Set1_ex14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("n=");
+            int n = int.Parse(Console.ReadLine());
+
+            int copiaLuiN = n;
+           
+            int nr = 0;
+
+            while( copiaLuiN > 0 )
+            {
+                int c = copiaLuiN % 10;
+                copiaLuiN /= 10;
+                nr = nr * 10 + c;
+                
+            }
+             
+            if ( nr == n)
+            {
+                Console.WriteLine("n este palindrom.");
+            }
+            else
+            {
+                Console.WriteLine("n NU este palindrom.");
+            }
+        }
+    }
+}
