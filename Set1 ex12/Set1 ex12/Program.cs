fdf97ca69f0f8c7cfa0d7a49632e6535b0ffedbe
DariaPetre <96527117+DariaPetre@users.noreply.github.com>
2024-01-16 20:11:58 +0200

    Add files via upload

---
+++ Set1 ex12/Set1 ex12/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Set1_ex12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("a=");
+            int a = int.Parse(Console.ReadLine());  
+            
+            Console.Write("b=");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.Write("n=");
+            int n = int.Parse(Console.ReadLine());
+
+            uint cnt = 0;
+            for(int i=a; i<=b; i++) 
+            { 
+              if(i % n == 0) cnt++;  
+            }
+
+            Console.WriteLine($"{cnt} numere din intervalul [a,b] sunt divizibile cu n.");
+
+        }
+    }
+}
