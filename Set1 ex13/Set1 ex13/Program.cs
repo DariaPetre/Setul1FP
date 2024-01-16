90b28fa309b2f9e99c518b28f38c3c752103ac9a
DariaPetre <96527117+DariaPetre@users.noreply.github.com>
2024-01-16 20:12:42 +0200

    Add files via upload

---
+++ Set1 ex13/Set1 ex13/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Set1_ex13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("y1=");
+            int y1 = int.Parse(Console.ReadLine());
+
+            Console.Write("y2=");
+            int y2 = int.Parse(Console.ReadLine());
+
+            int cnt = 0;
+
+            for(int i = y1+1; i <= y2-1; i++)
+            {
+                bool anBisect = DateTime.IsLeapYear(i);
+                if (anBisect)
+                {
+                    cnt++;
+                }
+            }
+
+            Console.WriteLine(cnt);
+
+        }
+    }
+}
