64b0d0ef6b05d5a3a946011e0b0b2fe7756d146e
DariaPetre <96527117+DariaPetre@users.noreply.github.com>
2024-01-16 20:07:18 +0200

    Add files via upload

---
+++ Set1 ex07/Set1 ex7/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Set1_ex7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("a=");
+            float a = float.Parse(Console.ReadLine());
+
+            Console.Write("b=");
+            float b = float.Parse(Console.ReadLine());
+
+            float aux;
+
+            aux = a;
+            a = b;
+            b = aux;
+
+            Console.WriteLine("Rezultat:");
+            Console.WriteLine($"a={a}");
+            Console.WriteLine($"b={b}");
+        }
+
+    }
+}
