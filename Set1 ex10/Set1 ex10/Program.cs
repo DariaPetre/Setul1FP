27a168caa156993d62f274d13b79360e15606b72
DariaPetre <96527117+DariaPetre@users.noreply.github.com>
2024-01-16 20:09:35 +0200

    Add files via upload

---
+++ Set1 ex10/Set1 ex10/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Set1_ex10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            if(prim(n)) 
+            {
+                Console.WriteLine("n este prim.");
+            }
+            else
+            {
+                Console.WriteLine("n NU este prim.");
+            }
+           
+
+        }
+        static bool prim(int n)
+        {
+            if (n == 0 || n == 1) return false;
+            if (n == 2) return true;
+            if (n % 2 == 0) return false;
+            for (int i = 3; i * i <= n; i += 2)
+            {
+                if (n % i == 0) return false;
+            }
+            return true;
+        }
+    }
+}
