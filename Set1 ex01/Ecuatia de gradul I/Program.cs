ea128c78694cce6ea8289b7b8ef178f06d4577ae
DariaPetre <96527117+DariaPetre@users.noreply.github.com>
2024-01-16 20:03:35 +0200

    Add files via upload

---
+++ Set1 ex01/Ecuatia de gradul I/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace Ecuatia_de_gradul_I
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //************ ECUATIA DE GRADUL I ********
+            //                  ax+b=0
+            Console.Write("a=");
+            float a = float.Parse(Console.ReadLine());
+
+            Console.Write("b=");
+            float b = float.Parse(Console.ReadLine());
+
+            if (a != 0)
+            {
+                float x;
+                x = -b / a;
+                Console.WriteLine(x);
+            }
+            else if (a == 0) 
+            {
+               if(b == 0)
+                {
+                    Console.WriteLine("x poate fi orice numar real.");
+                }
+                else
+                { 
+                    Console.WriteLine("Ecuatia nu are solutie.");
+                }
+            
+            }
+
+        }
+    }
+}
