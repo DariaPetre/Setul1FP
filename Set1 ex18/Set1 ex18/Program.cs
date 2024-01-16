9257a5b3733a42d9d35ce62a76c2e72f59ebc608
DariaPetre <96527117+DariaPetre@users.noreply.github.com>
2024-01-16 20:16:15 +0200

    Add files via upload

---
+++ Set1 ex18/Set1 ex18/Program.cs
@@ -0,0 +1,32 @@
+﻿
+namespace Set1_ex18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("n=");
+            int n = int.Parse(Console.ReadLine());
+
+            int FactorPrim = 2;
+
+            while (n > 1) 
+            {
+                int exponent = 0;
+                while ( n % FactorPrim == 0 )
+                {
+                    exponent++;
+                    n /= FactorPrim;
+                }
+
+                if ( exponent != 0 )
+                   Console.Write($"{FactorPrim}^{exponent}");
+
+                if (n > 1 && exponent !=0 ) Console.Write("x");
+
+                FactorPrim++;
+                
+            }
+        }
+    }
+}
