7826a2acb1e39b173c5ba8ac75fcf6843f6c6710
DariaPetre <96527117+DariaPetre@users.noreply.github.com>
2024-01-17 13:34:42 +0200

    Update Program.cs

--- Set1 ex04/Set1 ex4/Program.cs
+++ Set1 ex04/Set1 ex4/Program.cs
@@ -1,22 +1,27 @@
-﻿using System;
+using System;
 namespace Set1_ex4
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-           //*******~  y este an bisect?  ~******
+            //*******~  y este an bisect?  ~******
+            Console.Write("y=");
            int y = Convert.ToInt32(Console.ReadLine());
-            bool anBisect = DateTime.IsLeapYear(y);
-            if (anBisect)
-            {
+           
+            if (esteBisect(y))
                 Console.WriteLine("y este an bisect.");
-            }
             else
-            {
                 Console.WriteLine("y NU este an bisect.");
-            }
 
+        }
+
+        private static bool esteBisect(int y)
+        {
+            if((y % 4 == 0 && y % 100 != 0) || y % 400 == 0)
+                return true;
+            else
+                return false;
         }
     }
 }
